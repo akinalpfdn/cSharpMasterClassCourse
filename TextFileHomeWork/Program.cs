148a9f26033e6b08adacb6510a509cc2f48212cf
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2023-06-06 23:39:40 +0300

    regex Example

--- TextFileHomeWork/Program.cs
+++ TextFileHomeWork/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TextFileHomeWork
@@ -11,6 +12,7 @@ namespace TextFileHomeWork
     {
         static void Main(string[] args)
         {
+            /*
             var lines = System.IO.File.ReadAllLines(@"C:\Users\akina\Downloads\input.txt");
             if(lines != null)
             {
@@ -24,15 +26,20 @@ namespace TextFileHomeWork
                 }
                 System.IO.File.WriteAllText(@"C:\Users\akina\Downloads\output.txt", output.Trim());
             }
+            */
             //Console.ReadKey();
+            var text = System.IO.File.ReadAllText(@"C:\Users\akina\Downloads\input2.txt");
+            Regex regex = new Regex(@"\d{2,3}");
+            MatchCollection matches = regex.Matches(text);
+            string result = string.Empty;
+            foreach (Match match in matches)
+            {
+                result += (char)int.Parse(match.Groups[0].Value);
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
 
-        public static void Main(string[] args)
-        {
-            string stringForFloat = "0.85"; // datatype should be float
-            string stringForInt = "12345"; // datatype should be int
-            float floatNumber = float.Parse(stringForFloat);
-            int inttNumber = int.Parse(stringForInt);
-        }
     }
+    
 }
